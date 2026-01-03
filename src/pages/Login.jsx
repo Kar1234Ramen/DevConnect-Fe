ba9b3fb280077dd42a270694bfe7cbfb9cd9a7c8
Ramen Kar <109908494+Kar1234Ramen@users.noreply.github.com>
2026-01-03 12:05:45 +0530

    Logout built and handled

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -10,6 +10,7 @@ const Login = () => {
   const [password, setPassword] = useState("");
   const dispatch = useDispatch();
   const navigate = useNavigate();
+  const [error, setError] = useState("");
 
   const loginHandler = async () => {
     try {
@@ -26,6 +27,7 @@ const Login = () => {
       dispatch(addUser(res.data));
       return navigate("/");
     } catch (err) {
+      setError(err?.response.data || err.message);
       console.error(err);
     }
   };
@@ -67,6 +69,7 @@ const Login = () => {
               />
             </label>
           </div>
+          {error && <p className="text-red-500">{error}</p>}
           <div className="card-actions justify-center m-2">
             <button className="btn btn-primary" onClick={loginHandler}>
               Login
