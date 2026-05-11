8b6a6253c088baac04eac43a612c7cc9f913b868
Ramen Kar <109908494+Kar1234Ramen@users.noreply.github.com>
2026-05-12 00:50:09 +0530

    fix socket.io connection

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -7,6 +7,6 @@ export const createSocketConnection = () => {
       withCredentials: true,
     });
   } else {
-    return io("/", { path: "/api/socket.io" });
+    return io("/", { path: "/api/socket.io", withCredentials: true });
   }
 };
